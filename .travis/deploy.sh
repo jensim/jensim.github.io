404d4ee7bb966add2664d1b6bae34e32798644c3
Jens Brimfors <jens.brimfors@nordnet.se>
2019-07-06 13:44:00 +0200

    Include README file in release to master

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,6 +6,7 @@ set -v
 . .travis/add_ssh_key.sh $1 $2
 
 cp README.md dist/jensim/README.md
+cp package.json dist/jensim/package.json
 cd dist/jensim
 git add -A
 git commit -m "Deploying successful build"
