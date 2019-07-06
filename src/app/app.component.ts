8edd0e3eae505740c5678847442eae755e5c9e62
Jens Brimfors <jens.brimfors@nordnet.se>
2019-07-06 13:27:20 +0200

    Added a basic menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('rotatedState', [
+      state('default', style({transform: 'rotate(0)'})),
+      state('rotated', style({transform: 'rotate(-355deg)'})),
+      transition('rotated => default', animate('400ms ease-out')),
+      transition('default => rotated', animate('400ms ease-in'))
+    ])
+  ],
 })
 export class AppComponent {
-  title = 'jensim';
+  title = 'Jensim';
+  opened = true;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {MaterialModule} from './modules/material-module/material.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HomeComponent} from './pages/home/home.component';
+import {HttpClientModule} from '@angular/common/http';
+import {MatNativeDateModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AngularFontAwesomeModule} from 'angular-font-awesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    AngularFontAwesomeModule,
+    BrowserModule,
+    MaterialModule,
+    FormsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
