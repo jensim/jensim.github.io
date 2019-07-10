cbcff9ad4fbacfabd7995dba2936af446a31265b
Jens Brimfors <jens.brimfors@nordnet.se>
2019-07-10 15:37:43 +0200

    Rework menu styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import {ActivatedRoute} from '@angular/router';
   animations: [
     trigger('rotatedState', [
       state('default', style({transform: 'rotate(0)'})),
-      state('rotated', style({transform: 'rotate(-355deg)'})),
+      state('rotated', style({transform: 'rotate(-360deg)'})),
       transition('rotated => default', animate('400ms ease-out')),
       transition('default => rotated', animate('400ms ease-in')),
     ]),
